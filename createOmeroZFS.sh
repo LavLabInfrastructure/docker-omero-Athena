858e36376711798d06bc7598ebb1e78fba8c00fa
barrettMCW <mjbarrett@mcw.edu>
2022-06-25 17:04:34 +0000

    preProd

--- createOmeroZFS.sh
+++ createOmeroZFS.sh
@@ -15,39 +15,35 @@ sudo echo "CREATING ZFS FILESYSTEM FOR OMERO" || (echo "must be run as root" && 
 	# with hard disks, compression is a nobrainer
 	# relatime for webapps
 	# with files this large and psql, caching causes more harm than good (?)
-	# redundant_metadata is iffy, it has a slight risk of total data loss but it is extremely minimal
 		# i'll only keep enabled for a good .1 sec latency reduction
 	# log bias? we don't have l2arc so idk if that even matters, maybe carve a section out of boot?
-zfs create -o xattr=sa -o compression=lz4 -o atime=off -o relatime=on -o redundant_metadata=most -o primarycache=metadata $1/omeroZFS
+zfs create -o xattr=sa -o compression=lz4 -o atime=off -o relatime=on -o primarycache=metadata $1/omeroZFS
 echo "created parent dataset"
 #give the permissions to omero-server
-chown -R 1000:997 /$1/omeroZFS
 
 # create set for ome.tiffs ##I REALLY want to try multi MB recordsizes
-zfs create -o recordsize=1M $1/omeroZFS/omeTIFF
+zfs create -o recordsize=1M -o primarycache=all -o relatime=off $1/omeroZFS/omeTIFF
 echo "created ometiff dir"
-chown -R 1000:997 /$1/omeroZFS/omeTIFF
 
 # create set for logs
 zfs create -o compression=gzip-1 -o primarycache=none $1/omeroZFS/logs
-echo "created log dir"
-chown -R 1000:997 /$1/omeroZFS/logs
+echo "created master log dir"
+mkdir /$1/omeroZFS/logs/server && chown -R 1000:997 /$1/omeroZFS/logs/server
+echo "made log dir for omero.server"
+mkdir /$1/omeroZFS/logs/postgres && chown -R 70:70 /$1/omeroZFS/logs/postgres
+echo "made log dir for postgres"
 
 # create set for omero server data
 	#no idea on whats best here
-zfs create $1/omeroZFS/server
+zfs create -o recordsize=8k $1/omeroZFS/server
 echo "created server dir"
 chown -R 1000:997 /$1/omeroZFS/server
 
 # create sets for psql
-	# someone recommends changing psql block to 32k and WAL to 64k and keeping 128k? worth a test
-		# maybe 64k recordsize with above changes?
-    # alpine postgres g/uid is 70
 	# mostly changes in psql itself
-zfs create $1/omeroZFS/postgre
+zfs create -o primarycache=none -o recordsize=8k $1/omeroZFS/postgre
+zfs create $1/omeroZFS/postgre/data
+zfs create $1/omeroZFS/postgre/wal
 chown -R 70:70 /$1/omeroZFS/postgre
-zfs create -o recordsize=32k $1/omeroZFS/postgre/data
-zfs create -o recordsize=64k $1/omeroZFS/postgre/wal
-chown -R 70:70 /$1/omeroZFS/postgre/*
 echo "created postgre dirs"
 echo "finished!"
